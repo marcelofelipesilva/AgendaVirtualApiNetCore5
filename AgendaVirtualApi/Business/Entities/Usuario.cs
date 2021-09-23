401c9a5529326844055c9bcaeaef9b0bd063792b
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-23 12:04:49 -0300

    add Usuario.cs

---
+++ AgendaVirtualApi/Business/Entities/Usuario.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AgendaVirtualApi.Business.Entities
+{
+    public class Usuario
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UsuarioID { get; set; }
+        [Required]
+        [StringLength(8)]
+        public string Login { get; set; }
+        [Required]
+        [StringLength(6)]
+        public string Senha { get; set; }   
+    }
+}
