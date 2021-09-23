83cb50c7bef1ee310421003163271cf1c8065ec5
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-23 12:16:54 -0300

    add AgendaVirtualApiDbContext.cs

---
+++ AgendaVirtualApi/Infra/Data/AgendaVirtualApiDbContext.cs
@@ -0,0 +1,30 @@
+using AgendaVirtualApi.Business.Entities;
+using AgendaVirtualApi.Infra.Data.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendaVirtualApi.Infra.Data
+{
+    public class AgendaVirtualApiDbContext : DbContext
+    {
+        public DbSet<Contato> Contatos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Tarefa> Tarefas { get; set; }
+
+        public AgendaVirtualApiDbContext(DbContextOptions<AgendaVirtualApiDbContext> options) : base(options)
+        {
+            
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ContatoMapping());
+            modelBuilder.ApplyConfiguration(new TarefaMapping());
+            modelBuilder.ApplyConfiguration(new UsuarioMapping());
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        { 
+            optionsBuilder.UseSqlServer(@"Server=172.17.0.6;Database=agandaVirtualApiBD;User Id=sa;Password=App@12345;");
+        }
+    }
+}
