c8104cabcab39792fd5e283385428817fc482a95
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-24 19:01:07 -0300

    add ContatoController.cs

---
+++ AgendaVirtualApi/Controllers/ContatoController.cs
@@ -0,0 +1,106 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AgendaVirtualApi.Business.Entities;
+using AgendaVirtualApi.Infra.Data;
+
+namespace AgendaVirtualApi.Controllers
+{
+    [Route("api/v1/contato")]
+    [ApiController]
+    public class ContatoController : ControllerBase
+    {
+        private readonly AgendaVirtualApiDbContext _context;
+
+        public ContatoController(AgendaVirtualApiDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Contato
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contato>>> GetContatos()
+        {
+            return await _context.Contatos.ToListAsync();
+        }
+
+        // GET: api/Contato/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contato>> GetContato(int id)
+        {
+            var contato = await _context.Contatos.FindAsync(id);
+
+            if (contato == null)
+            {
+                return NotFound();
+            }
+
+            return contato;
+        }
+
+        // PUT: api/Contato/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutContato(int id, Contato contato)
+        {
+            if (id != contato.ContatoID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(contato).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContatoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Contato
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Contato>> PostContato(Contato contato)
+        {
+            _context.Contatos.Add(contato);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetContato", new { id = contato.ContatoID }, contato);
+        }
+
+        // DELETE: api/Contato/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteContato(int id)
+        {
+            var contato = await _context.Contatos.FindAsync(id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+
+            _context.Contatos.Remove(contato);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ContatoExists(int id)
+        {
+            return _context.Contatos.Any(e => e.ContatoID == id);
+        }
+    }
+}
