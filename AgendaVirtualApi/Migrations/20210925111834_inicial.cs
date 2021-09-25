6a8fb8452d465ff16bdfdaf312cc79ffa6effb25
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-25 08:20:10 -0300

    add migrations inicial

---
+++ AgendaVirtualApi/Migrations/20210925111834_inicial.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AgendaVirtualApi.Migrations
+{
+    public partial class inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TB_CONTATO",
+                columns: table => new
+                {
+                    ContatoID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Telefone = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TB_CONTATO", x => x.ContatoID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TB_TAREFA",
+                columns: table => new
+                {
+                    TarefaID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    dataEntrega = table.Column<string>(type: "nvarchar(35)", maxLength: 35, nullable: true),
+                    Descricao = table.Column<string>(type: "nvarchar(350)", maxLength: 350, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TB_TAREFA", x => x.TarefaID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TB_USUARIO",
+                columns: table => new
+                {
+                    UsuarioID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Login = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: false),
+                    Senha = table.Column<string>(type: "nvarchar(6)", maxLength: 6, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TB_USUARIO", x => x.UsuarioID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TB_CONTATO");
+
+            migrationBuilder.DropTable(
+                name: "TB_TAREFA");
+
+            migrationBuilder.DropTable(
+                name: "TB_USUARIO");
+        }
+    }
+}
