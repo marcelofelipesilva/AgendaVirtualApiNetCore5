116ba4970e936941457316fab6c10c5c41584884
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-25 08:14:37 -0300

    add TarefaViewModelOutput.cs

---
+++ AgendaVirtualApi/Models/Tarefa/TarefaViewModelOutput.cs
@@ -0,0 +1,9 @@
+namespace AgendaVirtualApi.Models.Tarefa
+{
+    public class TarefaViewModelOutput
+    {
+        public string Nome { get; set; }
+        public string dataEntrega { get; set; }
+        public string Descricao { get; set; }
+    }
+}
